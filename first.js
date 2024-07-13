85966012ffd113c1fabbe7c989f5ea1c3e55b082
Amresh Kumar <amreshnew77@gmail.com>
2024-07-13 19:59:19 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,17 @@
+var num = 23;
+console.log(num);
+let str = "amresh";
+console.log(str);
+const temp =  true;
+console.log(temp);
+
+console.log(typeof(num));
+
+console.log(typeof(str));
+
+console.log(typeof(temp));
+
+str = "ankit";
+console.log(str);
+ temp =  false;
+console.log(temp);
